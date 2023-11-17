a1b1c7af145b060355dafb10e0b5d3d4c50f9d11
LocTM <tranminhloc.anhk@gmail.com>
2023-11-17 16:24:31 +0700

    BT2

---
+++ index.js
@@ -0,0 +1,22 @@
+// BT1
+// myColor = ["red", "green", "blue"]
+// myColor.push("black")
+// console.log(myColor)
+
+// BT2
+// var num=window.prompt();
+// var str = num.toString();
+// var result = [str[0]];
+//
+// for(var x=1; x<str.length; x++)
+//   {
+//     if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+//      {
+//       result.push('-', str[x]);
+//      }
+//     else
+//      {
+//       result.push(str[x]);
+//      }
+//   }
+// console.log(result.join(''));
