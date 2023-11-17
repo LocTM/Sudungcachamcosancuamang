e6e2b8aaaccad45a0a7e76f16e0d5bcfb0c39fb2
LocTM <tranminhloc.anhk@gmail.com>
2023-11-17 16:31:08 +0700

    BT3

--- index.js
+++ index.js
@@ -20,3 +20,26 @@
 //      }
 //   }
 // console.log(result.join(''));
+
+// BT3
+// str = prompt("Nhap vao day 1 dong chu bat ki:");
+// var UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var LOWER = 'abcdefghijklmnopqrstuvwxyz';
+// var result = [];
+//
+// for(var x=0; x<str.length; x++)
+// {
+//     if(UPPER.indexOf(str[x]) !== -1)
+//     {
+//         result.push(str[x].toLowerCase());
+//     }
+//     else if(LOWER.indexOf(str[x]) !== -1)
+//     {
+//         result.push(str[x].toUpperCase());
+//     }
+//     else
+//     {
+//         result.push(str[x]);
+//     }
+// }
+// document.write(result.join(''));
